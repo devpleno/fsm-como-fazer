b873fc7ef0ccb2a0b9a60d7a615f7a27e1453b3e
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-03-24 17:17:58 -0300

    Adicionando Bootstrap e Publicacoes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const axios = require('axios')
 const bodyParser = require('body-parser')
 
 const categorias = require('./routes/categorias')
+const publicacoes = require('./routes/publicacoes')
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded())
@@ -11,12 +12,11 @@ app.use(bodyParser.urlencoded())
 const port = process.env.PORT || 3000
 
 app.get('/', async (request, response) => {
-  const content = await axios.get('https://como-fazer-devpleno-lab.firebaseio.com/teste.json')
-  console.log(content.data)
-  response.render('index', { i: content.data })
+  response.render('index')
 })
 
 app.use('/categorias', categorias)
+app.use('/publicacoes', publicacoes)
 
 app.listen(port, err => {
   if (err) {
